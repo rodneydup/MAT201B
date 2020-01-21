1cbdc9171cedd0ef867467d861d68d3c1e613234
Rodney DuPlessis <rodney.dup@hotmail.com>
2020-01-21 04:55:24 -0800

    pixelRick2-electricBoogaloo

--- Assignment1/pixelRick.cpp
+++ Assignment1/pixelRick.cpp
@@ -11,6 +11,7 @@ struct MyApp : App {
   std::vector<Vec3f> poseHSV;
   std::vector<float> explode;
   int counter;
+  float gravity;
 
   void onCreate() override {
     mesh.primitive(Mesh::POINTS);
@@ -58,22 +59,81 @@ struct MyApp : App {
         mesh.vertices()[i].lerp(poseHSV[i], 0.02);
       }
     } else if (pixbehaviour == 4) {
-      if (counter > 0)
+      if (counter > -60)
         counter -= 1;
+      if (gravity > -2)
+        gravity -= 0.01;
+      for (int i = 0; i < mesh.vertices().size(); i++) {
+        rnd::Random<> rng;
+        float r = rng.gaussian();
+        float r2 = rng.gaussian();
+        float dist = mesh.vertices()[i].sumAbs();
+        if (counter > 0) {
+          mesh.vertices()[i].lerp(mesh.vertices()[i].rotate(0.02 / dist * r),
+                                  0.1);
+          mesh.vertices()[i].lerp(0, 0.02);
+        }
+        if (counter < 0 && counter > -60) {
+          gravity = 1;
+          mesh.vertices()[i].lerp(
+              Vec3f((explode[i] * 2 - 1) + (r * 4), gravity + r2, 0), 0.1);
+        }
+        if (counter == -60) {
+          mesh.vertices()[i].lerp(
+              Vec3f((explode[i] * 2 - 1) + (r * 4), gravity + r2, 0), 0.0005);
+        }
+      }
+    } else if (pixbehaviour == 5) {
+
       for (int i = 0; i < mesh.vertices().size(); i++) {
         rnd::Random<> rng;
         float r = rng.uniform();
         float dist = mesh.vertices()[i].sumAbs();
-        // mesh.vertices()[i].lerp(Vec3f((mesh.vertices()[i].x * cos(M_PI * dist + r)) -
-        //                                   (mesh.vertices()[i].y * sin(M_PI * dist + r)),
-        //                               (mesh.vertices()[i].x * sin(M_PI * dist + r)) -
-        //                                   (mesh.vertices()[i].y * cos(M_PI * dist + r)),
-        //                               0),
-        //                         0.01);
-        mesh.vertices()[i].lerp(mesh.vertices()[i].rotate(0.01 * dist), 0.1);
+        mesh.vertices()[i].lerp(mesh.vertices()[i].rotate(0.02 / dist * r),
+                                0.1);
+      }
+    } else if (pixbehaviour == 6) {
+      for (int i = 0; i < mesh.vertices().size(); i++) {
+        rnd::Random<> rng;
+        float r = rng.uniform();
+        float dist = mesh.vertices()[i].sumAbs();
+        mesh.vertices()[i].lerp(
+            Vec3f((mesh.vertices()[i].x * cos(M_PI / dist + r)) -
+                      (mesh.vertices()[i].y * sin(M_PI / dist + r)),
+                  (mesh.vertices()[i].x * sin(M_PI / dist + r)) -
+                      (mesh.vertices()[i].y * cos(M_PI / dist + r)),
+                  0),
+            0.01);
+      }
+    } else if (pixbehaviour == 7) {
+      for (int i = 0; i < mesh.vertices().size(); i++) {
+        rnd::Random<> rng;
+        float r = rng.uniform();
+        float dist = mesh.vertices()[i].sum();
+        mesh.vertices()[i].lerp(
+            Vec3f((mesh.vertices()[i].x * cos(M_PI / dist + r)) -
+                      (mesh.vertices()[i].y * sin(M_PI / dist + r)),
+                  (mesh.vertices()[i].x * sin(M_PI / dist + r)) -
+                      (mesh.vertices()[i].y * cos(M_PI / dist + r)),
+                  0),
+            0.01);
+      }
+    } else if (pixbehaviour == 8) {
+      for (int i = 0; i < mesh.vertices().size(); i++) {
+        rnd::Random<> rng;
+        float r = rng.uniform();
+        float dist = mesh.vertices()[i].sumAbs();
+        mesh.vertices()[i].lerp(
+            Vec3f((mesh.vertices()[i].x * cos(M_PI * dist + r)) -
+                      (mesh.vertices()[i].y * sin(M_PI * dist + r)),
+                  (mesh.vertices()[i].x * sin(M_PI * dist + r)) -
+                      (mesh.vertices()[i].y * cos(M_PI * dist + r)),
+                  0),
+            0.01);
       }
     }
     nav().faceToward(0);
+    std::cout << gravity << std::endl;
   }
 
   void onDraw(Graphics &g) override {
@@ -95,8 +155,20 @@ struct MyApp : App {
       pixbehaviour = 3;
       break;
     case '4':
-      pixbehaviour = 4;
       counter = 180;
+      pixbehaviour = 4;
+      break;
+    case '5':
+      pixbehaviour = 5;
+      break;
+    case '6':
+      pixbehaviour = 6;
+      break;
+    case '7':
+      pixbehaviour = 7;
+      break;
+    case '8':
+      pixbehaviour = 8;
       break;
     default:
       break;
