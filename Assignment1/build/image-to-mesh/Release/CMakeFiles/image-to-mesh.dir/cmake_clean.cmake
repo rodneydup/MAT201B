e8a2a41478eea8c388ccb117fc0313ca72213a5c
Rodney DuPlessis <rodney.dup@hotmail.com>
2020-01-20 18:27:54 -0800

    create assingment 1

---
+++ Assignment1/build/image-to-mesh/Release/CMakeFiles/image-to-mesh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image-to-mesh.dir/rodneydup/Assignment1/image-to-mesh.cpp.o"
+  "../../../bin/image-to-mesh.pdb"
+  "../../../bin/image-to-mesh"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image-to-mesh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Assignment1/build/image-to-mesh/Release/CMakeFiles/image-to-mesh_run.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image-to-mesh_run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/image-to-mesh_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
